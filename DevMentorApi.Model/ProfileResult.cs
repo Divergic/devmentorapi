f706856b0ec398431376522308f2d1cfd16374cf
Rory Primrose <rory@neovolve.com>
2017-08-14 16:21:02 +1000

    Updated ProfileStore to return ProfileResults from stored profiles.

---
+++ DevMentorApi.Model/ProfileResult.cs
@@ -0,0 +1,23 @@
+﻿namespace DevMentorApi.Model
+{
+    using System;
+
+    public class ProfileResult
+    {
+        public int? BirthYear { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Gender { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public ProfileStatus Status { get; set; }
+
+        public string TimeZone { get; set; }
+        
+        public int? YearStartedInTech { get; set; }
+    }
+}
