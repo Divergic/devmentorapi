b2990dff575db155fc3ee6aed408c5ffc1fea846
Rory Primrose <rory@neovolve.com>
2017-10-18 12:46:47 +1100

    Added retry logic to AccountProfileController.Get.

--- TechMentorApi.UnitTests/Controllers/AccountProfileControllerTests.cs
+++ TechMentorApi.UnitTests/Controllers/AccountProfileControllerTests.cs
@@ -11,7 +11,6 @@
     using Microsoft.AspNetCore.Routing;
     using ModelBuilder;
     using NSubstitute;
-    using TechMentorApi.Business;
     using TechMentorApi.Business.Commands;
     using TechMentorApi.Business.Queries;
     using TechMentorApi.Controllers;
@@ -21,6 +20,77 @@
 
     public class AccountProfileControllerTests
     {
+        [Fact]
+        public async Task GetAttemptsToGetProfileMultipleTimesTest()
+        {
+            var account = Model.Create<Account>();
+            var profile = Model.Create<Profile>();
+            var user = ClaimsIdentityFactory.BuildPrincipal(account);
+
+            var query = Substitute.For<IProfileQuery>();
+            var command = Substitute.For<IProfileCommand>();
+            var httpContext = Substitute.For<HttpContext>();
+
+            httpContext.User = user;
+
+            var routerData = new RouteData();
+            var actionDescriptor = new ControllerActionDescriptor();
+            var actionContext = new ActionContext(httpContext, routerData, actionDescriptor);
+            var controllerContext = new ControllerContext(actionContext);
+
+            using (var tokenSource = new CancellationTokenSource())
+            {
+                query.GetProfile(account.Id, tokenSource.Token).Returns(null, null, null, profile);
+
+                using (var target = new AccountProfileController(query, command))
+                {
+                    target.ControllerContext = controllerContext;
+
+                    var actual = await target.Get(tokenSource.Token).ConfigureAwait(false);
+
+                    actual.Should().BeOfType<OkObjectResult>();
+
+                    var result = actual.As<OkObjectResult>();
+
+                    result.Value.ShouldBeEquivalentTo(profile);
+
+                    await query.Received(4).GetProfile(account.Id, tokenSource.Token).ConfigureAwait(false);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task GetReturnsNotFoundAfterAllRetryAttemptsFailTest()
+        {
+            var account = Model.Create<Account>();
+            var user = ClaimsIdentityFactory.BuildPrincipal(account);
+
+            var query = Substitute.For<IProfileQuery>();
+            var command = Substitute.For<IProfileCommand>();
+            var httpContext = Substitute.For<HttpContext>();
+
+            httpContext.User = user;
+
+            var routerData = new RouteData();
+            var actionDescriptor = new ControllerActionDescriptor();
+            var actionContext = new ActionContext(httpContext, routerData, actionDescriptor);
+            var controllerContext = new ControllerContext(actionContext);
+
+            using (var tokenSource = new CancellationTokenSource())
+            {
+                using (var target = new AccountProfileController(query, command))
+                {
+                    target.ControllerContext = controllerContext;
+
+                    var actual = await target.Get(tokenSource.Token).ConfigureAwait(false);
+
+                    actual.Should().BeOfType<ErrorMessageResult>();
+
+                    await query.Received(4).GetProfile(account.Id, tokenSource.Token).ConfigureAwait(false);
+                }
+            }
+        }
+
         [Fact]
         public async Task GetReturnsNotFoundWhenManagerReturnsNullTest()
         {
